7fcc66e6ed2deff7fa0bee71b85ba8ce431eaf98
LuisRochaEli <luis.rocha@elizondomx.com>
2024-10-07 11:08:34 -0600

    Movimientos

--- src/helpers/httpSINELI.ts
+++ src/helpers/httpSINELI.ts
@@ -47,7 +47,7 @@ httpInstanceDocumentos.interceptors.request.use(
 );
 
 const httpInstanceCreditos = axios.create({
-  baseURL: 'https://localhost:7096/api'//`https://www.elizondo.mx:5050/<%Ambiente%>/AppCreditoTest`,
+  baseURL: `https://www.elizondo.mx:5050/<%Ambiente%>/AppCreditoTest`,
 });
 
 httpInstanceCreditos.interceptors.request.use(
